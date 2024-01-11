3d558e3eed6693fd31e3076bcf83deb69b663c2d
elena.lazareva <eknyle@yandex.ru>
2024-01-11 12:43:49 +0300

    task2. another approach

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,3 +1,3 @@
 // code
 let number1 = Math.floor(Math.random() * 100);
-console.log(number1+':'+(number1%2===0?'четное число':'нечетное число'));
+console.log(number1+':'+(number1 % 2===0?'четное число':'нечетное число'));
